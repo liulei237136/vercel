f7c9ea9f551823435ff5a55958a1688e577e3a7e
liu lei <liulei237136@gmail.com>
2022-06-07 17:59:54 +0800

    1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -43,7 +43,7 @@ export default async function handler(req, res) {
     res.setHeader('Content-Type', 'audio/mepg');
     // res.setHeader('Accept-Ranges', 'bytes');
     // res.setHeader('Content-Length', '81132');
-    res.status(200).end(data);
+    // res.status(200).end(data);
     resInner.pipe(res);
 
   } catch (e) {
