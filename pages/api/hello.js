e1885e1c45ea2b5ef64cc8b3e430ffa3b54eb18e
liu lei <liulei237136@gmail.com>
2022-06-07 16:49:39 +0800

    remove blob

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -5,10 +5,11 @@ export default async function handler(req, res) {
   const result = await axios({
     url: 'https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3',
     method: 'GET',
-    responseType: 'blob', // important
+    // responseType: 'blob', // important
   });
 
   const data = result.data;
 
   res.status(200).send(data);
+  // res
 }
