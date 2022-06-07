d2c990e074e3b1781e75b419a86cdcf7a89fdec8
liu lei <liulei237136@gmail.com>
2022-06-07 17:38:36 +0800

    1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -22,23 +22,29 @@ export default async function handler(req, res) {
   // res.setHeader('Content-Length', '81132');
   // res.status(200).end(data);
   // res
-  const data = await new Promise((resolve, reject) => {
-    http.get('https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3', (res) => {
-      // if (res.statusCode !== 200) {
-      //   return reject('');
-      // }
-      let data = '';
-      res.on('data', (chunk) => {
-        data += chunk.toString();
-      });
-      res.on('end', () => {
-        return resolve(data);
+  try {
+    const data = await new Promise((resolve, reject) => {
+      http.get('https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3', (res) => {
+        if (res.statusCode !== 200) {
+          return reject('error');
+        }
+        let data = '';
+        res.on('data', (chunk) => {
+          data += chunk.toString();
+        });
+        res.on('end', () => {
+          return resolve(data);
+        });
       });
     });
-  });
 
-  res.setHeader('Content-Type', 'audio/mepg');
-  // res.setHeader('Accept-Ranges', 'bytes');
-  // res.setHeader('Content-Length', '81132');
-  res.status(200).send(data);
+    res.setHeader('Content-Type', 'audio/mepg');
+    res.setHeader('Accept-Ranges', 'bytes');
+    res.setHeader('Content-Length', '81132');
+    res.status(200).send(data);
+
+  } catch (e) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.status(500).send(e.message);
+  }
 }
