9e7dac8029e882b2ce45fae66b33c1d68481ab89
liu lei <liulei237136@gmail.com>
2022-06-07 17:25:29 +0800

    1

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,22 +1,44 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from 'axios';
+const http = require('http');
+
+
 
 export default async function handler(req, res) {
-  const result = await axios({
-    url: 'https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3',
-    method: 'GET',
-    responseType: 'blob', // important
+  // const result = await axios({
+  //   url: 'https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3',
+  //   method: 'GET',
+  //   responseType: 'blob', // important
+  // });
+
+  // const data = result.data;
+
+  //   Accept-Ranges: bytes
+  // Connection: keep-alive
+  // Content-Length: 81132
+
+  // res.setHeader('Content-Type', 'audio/mepg');
+  // res.setHeader('Accept-Ranges', 'bytes');
+  // res.setHeader('Content-Length', '81132');
+  // res.status(200).end(data);
+  // res
+  const data = await  new Promise((resolve,reject)=>{
+    http.get('https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3', (res) => {
+      if (res.statusCode !== 200) {
+        return reject('');
+      }
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk.toString();
+      });
+      res.on('end', () => {
+        return resolve(data);
+      });
+    });
   });
 
-  const data = result.data;
-
-//   Accept-Ranges: bytes
-// Connection: keep-alive
-// Content-Length: 81132
-
-  res.setHeader('Content-Type', 'audio/mepg');
-  res.setHeader('Accept-Ranges', 'bytes');
-  res.setHeader('Content-Length', '81132');
-  res.status(200).end(data);
-  // res
+   res.setHeader('Content-Type', 'audio/mepg');
+  // res.setHeader('Accept-Ranges', 'bytes');
+  // res.setHeader('Content-Length', '81132');
+  res.status(200).end(data); 
 }
