266ba80032fd2180adf2d5016ee5a92ed26df6ea
liu lei <liulei237136@gmail.com>
2022-06-07 16:30:37 +0800

    test 1.mp3

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import axios from 'axios';
 
-export default function handler(req, res) {
-  res.status(200).json({ name: '1' })
+export default async function handler(req, res) {
+  const result = await axios({
+    url: 'https://diandu-1307995562.cos.ap-hongkong.myqcloud.com/1.mp3',
+    method: 'GET',
+    responseType: 'blob', // important
+  });
+
+  const data = result.data;
+
+  res.status(200).send(data);
 }
